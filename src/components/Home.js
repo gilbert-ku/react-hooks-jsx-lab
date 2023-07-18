7134763f204d372ae6c74f5d59d44705fffcf5dd
gilbert-ku <gilbert45ku@gmail.com>
2023-07-18 23:43:40 +0300

    completed assignment

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,20 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
+
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  const myStyle = {
+    color: "firebrick"
+  };
+
+  return (
+    <div id="home">
+      <h1 style={myStyle}>
+        {name} is a Web Developer from {city}
+      </h1>
+    </div>
+  );
 }
 
 export default Home;
